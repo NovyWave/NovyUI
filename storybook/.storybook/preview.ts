ac9891ccb1a0791d39eef387544bf4a3bcae798e
Martin Kavík <martin@kavik.cz>
2025-05-15 21:40:40 +0200

    dark theme

--- storybook/.storybook/preview.ts
+++ storybook/.storybook/preview.ts
@@ -2,10 +2,23 @@ import type { Preview } from '@storybook/vue3'
 
 const preview: Preview = {
   parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        { name: 'light', value: '#ffffff' },
+        { name: 'dark', value: '#222222' },
+      ],
+    },
+    darkMode: {
+      current: 'dark',
+      darkClass: 'dark',
+      lightClass: 'light',
+      stylePreview: true,
+    },
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
